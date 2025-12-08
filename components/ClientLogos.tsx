6400fe90f7f0757a7dd05230d0f00cd785f60714
worldofprasanna <prasannacse2005@gmail.com>
2025-12-08 19:24:53 +0530

    updated client logos

--- components/ClientLogos.tsx
+++ components/ClientLogos.tsx
@@ -7,16 +7,18 @@ export default function ClientLogos() {
         Our Esteemed Clients
       </p>
       <div className="mt-2 flex flex-wrap justify-center items-center gap-6 opacity-70">
-        <Image src="/dacio-logo.png" alt="dacio" width={70} height={28} className="h-5 w-auto" />
-        <Image src="/finin.png" alt="finin" width={84} height={28} className="h-5 w-auto" />
-        <Image src="/nd.png" alt="nd" width={70} height={28} className="h-5 w-auto" />
-        <Image src="/taxnodes.png" alt="taxnodes" width={98} height={28} className="h-5 w-auto" />
-        <Image src="/politexts.jpg" alt="politexts" width={98} height={28} className="h-5 w-auto" />
-        <Image src="/merchantspring_logo.jpeg" alt="merchantsprings" width={68} height={28} className="h-12 w-auto" />
-        <Image src="/backsie.png" alt="backsie" width={68} height={28} className="h-6 w-auto" />
-        <Image src="/svaksha.png" alt="svaksha" width={68} height={28} className="h-6 w-auto" />
-        <Image src="/hashconnect.jpeg" alt="hashconnect" width={68} height={28} className="h-12 w-auto" />
-        <Image src="/labfinder.png" alt="labfinder" width={68} height={28} className="h-5 w-auto" />
+        <Image src="/clients/dacio.png" alt="dacio" width={200} height={68} className="h-6 w-auto" />
+        <Image src="/clients/finin.png" alt="finin" width={200} height={68} className="h-6 w-auto" />
+        <Image src="/clients/nd.png" alt="nd" width={200} height={68} className="h-6 w-auto" />
+        <Image src="/clients/taxnodes.png" alt="taxnodes" width={200} height={68} className="h-6 w-auto" />
+        <Image src="/clients/politexts.png" alt="politexts" width={200} height={68} className="h-6 w-auto" />
+        <Image src="/clients/merchantspring.png" alt="merchantsprings" width={200} height={68} className="h-6 w-auto" />
+        <Image src="/clients/backsie.png" alt="backsie" width={200} height={68} className="h-6 w-auto" />
+        <Image src="/clients/svaksha.png" alt="svaksha" width={200} height={68} className="h-6 w-auto" />
+        <Image src="/clients/hashmato.png" alt="hashmato" width={200} height={68} className="h-5 w-auto" />
+        <Image src="/clients/labfinder.png" alt="labfinder" width={200} height={68} className="h-10 w-auto" />
+        <Image src="/clients/IIID.png" alt="IIID" width={200} height={68} className="h-8 w-auto" />
+        <Image src="/clients/bespoke.png" alt="bespoke" width={200} height={68} className="h-8 w-auto" />
       </div>
     </div>
   )
